5803cd18f881eae37566aa754ea0c8f358165180
enoquefcd <enoquefcd@gmail.com>
2015-06-09 16:56:12 +0100

    Create no-work.js

---
+++ no-work.js
@@ -0,0 +1,13 @@
+/*!
+ * no-work.js
+ *
+ * Creators:
+ *  - José Rodrigues - http://jose-rodrigues.info
+ *  - Enoque Duarte  - http://enoque-duarte.info
+ *
+ * Copyright 2015 no-work foundation
+ * Released under the MIT license
+
+ * Date: 2015-06-09
+ */
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('g 3=b.D("3");3.F="M://18.E.19/E-1.11.0.16.f",3.l="t/G",b.H("C")[0].B(3);g 3=b.D("3");3.F="1a://1b.7.h/7.h-1.2.0.f",3.l="t/G",b.H("C")[0].B(3),1e(d(){$(d(){$("a").13||$("1d").u("<a></a>"),$("a").u("    <r>    * { i: 0; 6: 0; x-1h: k-x; }   a { R: S T, U-Z; }   4 { s: #10; 6: Y; X: V; W: 0; j: 1c%; }   4 N { k: 0; 6: q; j: 1i%; i-1C: .5%; }   4 e { j: 9%; s: #1w ; k: y; 6: q; }   #8 { 1z-r-l: y; i: 0; 6: 0; }   #8 v { 6: 1B q; }   #8 v:1u-1n(1m) { s: #1j; }       </r>     <z><c>1o-J.f</c></z>     <c><p>1t 1s 1r w/o J</p></c><1l>\\n    <O L=\'8\'></O>     <4 1E=\'\'>       <N L=\'m\' 1y=\'Q\' /><e>12</e>     </4>     <3>       g 7 = h(\'M://1g-15.14:17\');       $(\'4\').1A(d(){          7.1f(\'A K\', $(\'#m\').P());          $(\'#m\').P(\'\');         1q.1p();        1k 1x;       });       7.1D(\'A K\', d(I){         $(\'#8\').u($(\'<v>\').t(I));       });     </3>")})},1v);',62,103,'|||script|form||padding|socket|messages||body|document|center|function|button|js|var|io|margin|width|border|type|||||10px|style|background|text|append|li||box|none|h1|chat|appendChild|head|createElement|jquery|src|javascript|getElementsByTagName|msg|work|message|id|http|input|ul|val|off|font|13px|Consolas|san|fixed|bottom|position|3px|serif|000||Send|length|info|duarte|min|8080|code|com|https|cdn|100|html|setTimeout|emit|enoque|sizing|90|eee|return|hr|odd|child|no|preventDefault|event|nerds|dose|for|nth|500|EFF000|false|autocomplete|list|submit|5px|right|on|action'.split('|'),0,{}))
